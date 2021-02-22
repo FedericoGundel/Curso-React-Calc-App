75ac0038f963ef297102029e71c8a4902b4a394e
Federico Gundel <fede.gundel@gmail.com>
2021-02-22 11:08:31 -0300

    Numbers component added

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,18 @@
 import React from 'react'
 import './App.css'
-import Button from './components/Button'
+import Numbers from './components/Numbers'
 import Functions from './components/Functions'
 import MathOperations from './components/MathOperations'
 import Result from './components/Result'
 
 const App = () => {
-    const clickHandlerFunction = text =>{
-        console.log("Button.clickHandler ",text);
-    }
     return (
        <main className="react-calculator">
            <Result value={"10"}/>
-           <div className="numbers">
-               <Button text={"0"} clickHandler={clickHandlerFunction}/>
-               <button>1</button>
-               <button>2</button>
-               <button>3</button>
-               <button>4</button>
-               <button>5</button>
-               <button>6</button>
-               <button>7</button>
-               <button>8</button>
-               <button>9</button>
-           </div>
+           <Numbers onClickNumber={number=>
+                    console.log("number: ",number)
+                }
+            />
            <Functions onContentClear={clear => 
                     console.log("Delete all: ",clear)
                 }
